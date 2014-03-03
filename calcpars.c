6974e8c8fca57e35a64fba2b4c1db84642aafaff
metaphor10 <borochov@student.american.edu>
2014-03-02 23:48:05 -0500

    tring to get str compare to work

--- calcpars.c
+++ calcpars.c
@@ -3,15 +3,19 @@
 #include <stdio.h>
 char currentToken;
 char previousToken;
+char variables [50][80];
+int valuesOfVariables[50];
+int locationInVariableArray;
 void getToken();
 void match(char,char*);
 void error(char*);
-char* tempVariableName;
+char tempVariableName[80];
 int locationInTemoVariableName;
 int insideVariable,insideDigit,insideStatment;
 
 void main()
 {
+	locationInVariableArray=0;
 	previousToken=-1;
 	insideDigit=0;
 	insideVariable=0;
@@ -27,21 +31,54 @@ void main()
 				locationInTemoVariableName=0;
 				printf("question mark" );
 				previousToken=currentToken;
-				while((currentToken=getchar())!=EOF)
+				while((currentToken=getchar())!=EOF && currentToken != ' ' && isalpha(currentToken))
 				{
-					if (isalpha(currentToken))
-					{
+				
 						printf("inside isalpha" );
-						tempVariableName[locationInTemoVariableName];
+						printf("%c\n", currentToken );
+						tempVariableName[locationInTemoVariableName]=currentToken;
 						locationInTemoVariableName++;
 						previousToken=currentToken;
-					}
+					
+					
 				}
-				printf(tempVariableName);
-				printf("%c\n",currentToken );
+				printf("%s, %s\n",tempVariableName,variables[0] );
+				if (isThereAVariable(tempVariableName))
+				{
+					printf("there is a variable %i, %i, %i\n",valuesOfVariables[isThereAVariable(tempVariableName)],isThereAVariable(tempVariableName),valuesOfVariables[0] );
+				}
+				
 			}else if (isalpha(currentToken))
 			{
+				locationInTemoVariableName=0;
+				tempVariableName[locationInTemoVariableName]=currentToken;
+				locationInTemoVariableName++;
+				while((currentToken=getchar())!=EOF && isalpha(currentToken) && currentToken != ' ')
+				{
+					tempVariableName[locationInTemoVariableName]=currentToken;
+						locationInTemoVariableName++;
+						previousToken=currentToken;
+				}
+				printf(" variable 0%s\n", variables[0]);
+				if (isThereAVariable(tempVariableName))
+				{
+					printf("is a match %c", currentToken);
+				}else
+				{
+					stringCopyForMe(locationInVariableArray,tempVariableName);
+					printf("%s\n",variables[locationInVariableArray] );
+					int sum=currentToken-'0';
+					while((currentToken=getchar())!=EOF && isdigit(currentToken))
+					{
+						sum = sum*10 + (currentToken-'0');
+
+					}
+					printf("%i\n", sum );
+					valuesOfVariables[locationInVariableArray]=sum;
+					locationInVariableArray++;
+				}
 				printf("is alpha" );
+
 			}else if (isdigit(currentToken))
 			{
 				printf("is digit" );
@@ -77,4 +114,35 @@ void match(char c, char* message){
 void error(char* message){
 	printf("parse error: %s\n", message);
 	exit(1);
+}
+int isThereAVariable(char * variableName){
+	int i;
+	for (i=0;i<50;i++)
+	{
+		if (strcmp(variableName,variables[i])==0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+int stringCopyForMe(int location, char* stringToCopy){
+	int i;
+	for (i=0;i<80 ;i++)
+	{
+		variables[location][i] = stringToCopy[i];
+	}
+}
+int stringCompareMine (int location, char * stringToCompare)
+{
+	int i=0;
+	int isTheSame=1;
+	while (i<80 && stringToCompare[i]!='\0')
+	{
+		if (variables[location][i]!=stringToCompare[i])
+		{
+			isTheSame=0;
+		}
+		i++;
+	}
 }
